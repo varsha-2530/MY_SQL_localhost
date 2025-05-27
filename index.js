4b2a8668920f7152c05d62d401e763a4a5792536
ADMIN <admin@ADMINs-MacBook-Pro.local>
2025-05-27 23:04:08 +0530

    adduser

--- index.js
+++ index.js
@@ -7,21 +7,6 @@ const PORT = 3000;
 app.use(express.json()); //accptes  json data
 
 
-// app.post('/addUser', (req, res)=>{
-//   const user =  {
-//     name:req.body.name,
-//     email:req.body.email,
-//     phone:req.body.phone,
-//     city:req.body.city,
-//   }
-//     //console.log(user);   
-//     const sql = `INSERT INTO 'employee' SET ?` 
-//     db.query(sql, user,(err, result)=>{
-//        if(err) console.log(err.sqlMessage);
-//         else res.json(user)
-//     })
-// });
-
 app.post("/addUser", (req, res)=>{
     const user = {
         name: req.body.name, 
